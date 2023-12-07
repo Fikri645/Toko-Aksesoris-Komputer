8ebbafef8e6196ba6fb047369661403028c13ca3
Fikri Wahidin <fikriayam4@gmail.com>
2023-12-08 06:42:27 +0700

    membuat halaman history

--- app/Http/Livewire/History.php
+++ app/Http/Livewire/History.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Livewire;
 
+use App\Pesanan;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class History extends Component
 {
+    public $pesanans; 
+
+    public function mount()
+    {
+        if (!Auth::user()) {
+            return redirect()->route('login');
+        }
+    }
     public function render()
     {
+        if (Auth::user()) {
+            $this->pesanans = Pesanan::where('user_id', Auth::user()->id)->where('status', '!=', 0)->get();
+        }
         return view('livewire.history');
     }
 }
--- resources/views/livewire/history.blade.php
+++ resources/views/livewire/history.blade.php
@@ -26,5 +26,72 @@
     </div>
 
     <div class="row mt-4">
-
+        <div class="col">
+            <div class="table-responsive">
+                <table class="table text-center">
+                    <thead>
+                        <tr>
+                            <td>No.</td>
+                            <td>Tanggal Pesan</td>
+                            <td>Kode Pemesanan</td>
+                            <td>Pesanan</td>
+                            <td>Status</td>
+                            <td>Total Harga</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @auth
+                        <?php $no = 1; ?>
+                        @forelse($pesanans as $pesanan)
+                            <tr>
+                                <td>{{ $no++ }}</td>
+                                <td>{{ $pesanan->created_at }}</td>
+                                <td>{{ $pesanan->kode_pemesanan }}</td>
+                                <td>
+                                    <?php $pesanan_details = \App\PesananDetail::where('pesanan_id', $pesanan->id)->get(); ?>
+                                    @foreach ($pesanan_details as $pesanan_detail)
+                                        <img src="{{ url('assets/produk') }}/{{ $pesanan_detail->product->gambar }}"
+                                            alt="{{ $pesanan_detail->product->gambar }}" class="img-fluid"
+                                            width="50">
+                                        {{ $pesanan_detail->product->nama }} <br>
+                                    @endforeach
+                                </td>
+                                <td>
+                                    @if ($pesanan->status == 1)
+                                        Belum Lunas
+                                    @else
+                                        Lunas
+                                    @endif
+                                </td>
+                                <td>Rp. {{ number_format($pesanan->total_harga + $pesanan->kode_unik) }}</td>
+                            </tr>
+                        @empty
+                            <tr>
+                                <td colspan="7">Data Kosong</td>
+                            </tr>
+                        @endforelse
+                        @endauth
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
+
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <p>Untuk pembayaran silahkan dapat transfer di rekening di bawah ini: </p>
+
+                    <div class="media">
+                        <img src="{{ url('assets/bri.png') }}" class="mr-3" alt="Bank BRI" width="60">
+                        <div class="media-body">
+                            <h5 class="mt-0">BANK BRI</h5>
+                            No. Rekening <strong>0001-01-012345-67-8</strong> a/n <strong>Ahmad Erpeel</strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
